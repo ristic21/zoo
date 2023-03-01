2ecdd3336478e3d43bca91d2c1d29604a717698a
vivify <vivify@vivifys-MacBook-Pro-5.local>
2023-03-01 19:12:15 +0100

    third commit

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -9,7 +9,7 @@ export class AnimalList extends Component {
         { id: 2, vrsta: 'medved', ime: "nekiMedved", datumRodjenja:new Date("2020/02/21").toISOString() },
         { id: 3, vrsta: 'lav', ime: "nekiLav", datumRodjenja:new Date("2020/04/21").toISOString() },
          { id: 4, vrsta: 'lav', ime: "nekiDrugiLav", datumRodjenja:new Date("2021/06/21").toISOString() },
-     { id: 5, vrsta: 'lav', ime: "nekiTreciLav", datumRodjenja:new Date("2021/04/21").toISOString() },
+     { id: 5, vrsta: 'lav', ime: "nekiTreciLav"},
     ]
     return(
 
@@ -19,7 +19,7 @@ export class AnimalList extends Component {
         <table className='zivotinjeTable'>
         <tbody>
 {nizZivotinja.map((zivotinja) => (
-          <tr className='zivotinjeTable' key={zivotinja.id}> <span>VRSTA: {zivotinja.vrsta} </span> <span>IME: {zivotinja.ime} </span> <span>DATUM RODJENJA: {zivotinja.datumRodjenja} </span> </tr>
+          <tr className='zivotinjeTable' key={zivotinja.id}> <span>VRSTA: {zivotinja.vrsta} </span> <span>IME: {zivotinja.ime} </span> <span>DATUM RODJENJA: {zivotinja.datumRodjenja ? zivotinja.datumRodjenja : "Datum rodjenja nepoznat"} </span> </tr>
         ))} 
 </tbody>
         </table>
